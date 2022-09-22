d982e4153bc092fcf3e9576b46808bc7976c2707
lyle-smith <smithl14@newpaltz.edu>
2022-09-22 15:09:04 -0400

    Created server folder and example server file

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const http = require('http'); // require is common js syntax. Returns http object with methods
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('Server running at http://' + hostname + ':' + port + '/');
+});
+
+
+const nnn = function(paranName) {
+  
+} // this keyword is complicated
+
+const obj = {
+  vvv() {
+    
+  }
+} // this keyword is complicated
+
+const fatArrow = x => x * 2;  // this keyword refers to the parent scope
