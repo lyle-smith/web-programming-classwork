c839a5e8fc1191afa5509eceff99d798cc96144b
lyle-smith <smithl14@newpaltz.edu>
2022-10-06 14:41:18 -0400

    Created module export example

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,27 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import ttt, * as math from "./stores/temp";
 
-const app = createApp(App)
+console.log({ ttt });
+const bigger = math.add(ttt, 2);
+console.log({ bigger });
 
-app.use(createPinia())
-app.use(router)
+let reducer = 0;
+if (reducer) {
+  const smaller = math.subtract(ttt, reducer);
+  console.log({ smaller });
+}
 
-app.mount('#app')
+import "bulma";
+import "./assets/main.scss";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
