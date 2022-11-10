32338b693de7e0f0f0644ead18417614e5b579e4
lyle-smith <smithl14@newpaltz.edu>
2022-11-10 14:38:07 -0500

    Added CORS handling

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 3000;
 
 // Send header
 app.use((req, res, next) => {
-  res.setHeader('SUNY', 'My SUNY');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
